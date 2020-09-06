445655733c1638f32fa5ad7db49c56a1140cc3b5
Wild <cadainmiller@gmail.com>
2020-09-06 14:03:22 -0500

    Still working

--- shop/src/app/component/components/dashboard/dashboard.module.ts
+++ shop/src/app/component/components/dashboard/dashboard.module.ts
@@ -1,15 +1,16 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
+import {
+  NgModule,
+  CUSTOM_ELEMENTS_SCHEMA,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../../shared/shared.module';
-
-
+import { AdminPanelComponent } from '../pages/admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [
-    CommonModule,SharedModule
-  ],
+  imports: [CommonModule, SharedModule, AdminPanelComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
-export class DashboardModule { }
+export class DashboardModule {}
--- shop/src/app/component/guard/auth.guard.ts
+++ shop/src/app/component/guard/auth.guard.ts
@@ -13,6 +13,17 @@ import { Observable } from 'rxjs';
 })
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
+
+  checkRole() {
+    const arr = localStorage.getItem('UserInfo');
+    const value = arr[3];
+
+    if (value != 'admin') {
+      return false;
+    } else {
+      return true;
+    }
+  }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -21,4 +32,18 @@ export class AuthGuard implements CanActivate {
     this.router.navigate(['/login']);
     return false;
   }
+
+  // canActivate(
+  //   next: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot
+  // ): boolean {
+  //   const arr = localStorage.getItem('UserInfo');
+  //   const value = arr[3];
+
+  //   if (localStorage.getItem('Token') && (value === 'admin') != null)
+  //     return true;
+
+  //   this.router.navigate(['/']);
+  //   return false;
+  // }
 }
