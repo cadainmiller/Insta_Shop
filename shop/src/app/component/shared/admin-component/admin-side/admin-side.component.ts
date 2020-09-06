b18d154b9412250d922ac44772783415435161c5
Wild <cadainmiller@gmail.com>
2020-09-06 14:36:11 -0500

    Finish for the day

--- shop/src/app/component/shared/admin-component/admin-side/admin-side.component.ts
+++ shop/src/app/component/shared/admin-component/admin-side/admin-side.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-admin-side',
   templateUrl: './admin-side.component.html',
-  styleUrls: ['./admin-side.component.scss']
+  styleUrls: ['./admin-side.component.scss'],
 })
 export class AdminSideComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
