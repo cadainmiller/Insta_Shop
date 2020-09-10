bfd62dcee5042a94af381a7733bb2246e0080d17
Cadain <cmiller@aventmark.com>
2020-09-10 07:47:45 -0500

    Working on the add product form

--- shop/src/app/component/shared/dialog/add-product/add-product.component.ts
+++ shop/src/app/component/shared/dialog/add-product/add-product.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import {IdGenerator} from 'src/app/component/analytics/idgenerator'
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.scss']
+  styleUrls: ['./add-product.component.scss'],
 })
 export class AddProductComponent implements OnInit {
-
   title: string;
   closeBtnName: string;
+  orderId = '';
 
-  constructor(public bsModalRef: BsModalRef) { }
+
+  constructor(public bsModalRef: BsModalRef, private idGenerator: IdGenerator) {}
 
   ngOnInit(): void {
-
+    console.log("PD" + this.idGenerator.uniqueId());
+    this.orderId = "PD" + this.idGenerator.uniqueId();
   }
-
 }
