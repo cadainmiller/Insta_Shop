d4fc7bb354bb173587619cd844d3005d04effb4e
Wild <cadainmiller@gmail.com>
2020-09-27 11:54:18 -0500

    Added data to order table

--- server/controller/orderController.js
+++ server/controller/orderController.js
@@ -17,7 +17,7 @@ exports.createOrder = async (req, res) => {
 
   try {
     let order = new Order({
-      orderId: "INV" + uniqueId(),
+      orderId: "ORD-" + uniqueId(),
       customer_info: req.body.customer_info,
       quantity: req.body.quantity,
       products: req.body.products,
--- shop/src/app/component/components/pages/admin-orders/admin-orders.component.ts
+++ shop/src/app/component/components/pages/admin-orders/admin-orders.component.ts
@@ -4,36 +4,30 @@ import { OrderService } from 'src/app/component/services/order.service';
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
-  styleUrls: ['./admin-orders.component.scss']
+  styleUrls: ['./admin-orders.component.scss'],
 })
 export class AdminOrdersComponent implements OnInit {
-
   config: any;
   collection = { count: 60, data: [] };
-  orders:any;
+  orders: [];
 
   constructor(private orderService: OrderService) {
-
-    
-    this.orderService.getAllOrder().subscribe((data => {
-      console.log(data)
-      this.collection.data = data;
-      //this.orders = data;
-
-    }))
+    this.orderService.getAllOrder().subscribe((data) => {
+      this.collection.data = data.Orders;
+      this.orders = data;
+      console.log(this.collection.data);
+    });
 
     this.config = {
       itemsPerPage: 13,
       currentPage: 1,
-      totalItems: this.collection.count
+      totalItems: this.collection.count,
     };
   }
 
-  pageChanged(event){
+  pageChanged(event) {
     this.config.currentPage = event;
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
