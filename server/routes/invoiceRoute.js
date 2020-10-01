325947297afdcc398fa047f9c46b288fbf2c965f
Wild Mobile <cadainmiller@gmail.com>
2020-09-30 19:30:23 -0500

    invoice route

--- server/routes/invoiceRoute.js
+++ server/routes/invoiceRoute.js
@@ -4,6 +4,7 @@ const invoiceController = require("../controller/invoiceController");
 const auth = require("../middleware/auth");
 
 router.post("/create", invoiceController.createInvoice);
+router.get("/:invoiceId", auth, invoiceController.getInvoiceById);
 router.get("/", auth, invoiceController.getInvoice);
 
 module.exports = router;
