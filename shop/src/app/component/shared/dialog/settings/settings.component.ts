0dd35f4011e101b28def6e51b9ba2221d6952062
Wild Mobile <cadainmiller@gmail.com>
2020-10-15 19:23:10 -0500

    Added service to get user information

--- shop/src/app/component/shared/dialog/settings/settings.component.ts
+++ shop/src/app/component/shared/dialog/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/component/services/user.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getProtectedData().subscribe((data) => {
+      console.log(data)
+    })
   }
 
 }
